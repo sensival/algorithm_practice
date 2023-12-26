2b63965377250d26ff879637910c1fa103507044
sensival <alstjs883@naver.com>
2023-12-26 23:00:38 +0900

    7-2 이진탐색 재귀호출

---
+++ algorithm_example/binarysearch/1_2_binsearch_recur.py
@@ -0,0 +1,25 @@
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) //2
+    
+    if array[mid] == target:
+        return mid
+    
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid-1)
+    
+    else:
+        return binary_search(array, target, mid+1, end)
+    
+# 원소의 개수 n, 찾고자하는 문자열 target
+n, target = list(map(int, input.split()))
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n-1)
+
+if result == None:
+    print("원소가 존재하지 않습니다. ")
+
+else:
+    print(result+1)
