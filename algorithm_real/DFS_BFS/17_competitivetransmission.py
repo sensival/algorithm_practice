8847613a8ce304c4baeff511f481b471f4b709c4
sensival <alstjs883@naver.com>
2024-01-23 23:55:32 +0900

    Q17 경쟁적 전염

--- algorithm_real/DFS_BFS/17_competitivetransmission.py
+++ algorithm_real/DFS_BFS/17_competitivetransmission.py
@@ -14,3 +14,48 @@
 # 0 0 0
 # 3 0 0
 # 1 2 2 출력 0
+
+from collections import deque
+
+n, k = map(int, input().split())
+
+graph = []
+data = []
+
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+    for j in range(n):
+        if graph[i][j] != 0:
+            # 바이러스 종류, 시간, X, Y 입력
+            data.append((graph[i][j], 0, i, j))
+
+# 낮은번호의 바이러스부터 큐에 넣기
+data.sort()
+q = deque(data)
+
+target_s, target_x, target_y = map(int, input().split())
+
+# 증식하는 네가지 방향
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+# 모든 큐의 원소하나씩 순회하는게 1초
+while q:
+    virus, s, x, y = q.popleft()
+
+    if s == target_s:
+        break
+    
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx and nx < n and 0 <= ny and ny < n:
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = virus
+                q.append((virus, s + 1, nx, ny)) # 전염시켰으니 다시 맨 뒤로
+
+
+
+print(graph[target_x-1][target_y-1])
