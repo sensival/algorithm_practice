4fd15b3c52bf1e3dcfcf7b24d973e1b5949e8058
sensival <alstjs883@naver.com>
2024-01-24 23:37:38 +0900

    Q20 감시피하기

---
+++ algorithm_real/DFS_BFS/20_Avoidsurveillance.py
@@ -0,0 +1,119 @@
+# NxN 크기의 복도가 있다. 복도는 1x1 크기의 칸으로 나누어지며, 특정한 위치에는 선생님, 학생, 혹은 장애물이 위치할 수 있다. 
+# 현재 몇 명의 학생들은 수업시간에 몰래 복도로 빠져나왔는데, 복도로 빠져나온 학생들은 선생님의 감시에 들키지 않는 것이 목표이다.
+# 각 선생님들은 자신의 위치에서 상, 하, 좌, 우 4가지 방향으로 감시를 진행한다. 단, 복도에 장애물이 위치한 경우, 선생님은 장애물 뒤편에 숨어 있는 학생들은 볼 수 없다. 
+# 또한 선생님은 상, 하, 좌, 우 4가지 방향에 대하여, 아무리 멀리 있더라도 장애물로 막히기 전까지의 학생들은 모두 볼 수 있다고 가정하자.
+# 학생들은 복도의 빈 칸 중에서 장애물을 설치할 위치를 골라, 정확히 3개의 장애물을 설치해야 한다. 
+# 결과적으로 3개의 장애물을 설치하여 모든 학생들을 감시로부터 피하도록 할 수 있는지 계산하고자 한다. 
+# NxN 크기의 복도에서 학생 및 선생님의 위치 정보가 주어졌을 때, 장애물을 정확히 3개 설치하여 모든 학생들이 선생님들의 감시를 피하도록 할 수 있는지 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 자연수 N이 주어진다. (3 ≤ N ≤ 6) 둘째 줄에 N개의 줄에 걸쳐서 복도의 정보가 주어진다. 각 행에서는 N개의 원소가 공백을 기준으로 구분되어 주어진다. 해당 위치에 학생이 있다면 S, 선생님이 있다면 T, 아무것도 존재하지 않는다면 X가 주어진다.
+
+# 단, 전체 선생님의 수는 5이하의 자연수, 전체 학생의 수는 30이하의 자연수이며 항상 빈 칸의 개수는 3개 이상으로 주어진다.
+
+# 출력
+# 첫째 줄에 정확히 3개의 장애물을 설치하여 모든 학생들을 감시로부터 피하도록 할 수 있는지의 여부를 출력한다. 모든 학생들을 감시로부터 피하도록 할 수 있다면 "YES", 그렇지 않다면 "NO"를 출력한다.
+
+# 예제 입력 1 
+# 5
+# X S X X T
+# T X S X X
+# X X X X X
+# X T X X X
+# X X T X X
+
+# 예제 출력 1 
+# YES
+
+# 예제 입력 2 
+# 4
+# S S S T
+# X X X X
+# X X X X
+# T T T X
+
+# 예제 출력 2 
+# NO
+
+from itertools import combinations
+
+n = int(input())
+board = []
+teachers = []
+spaces = []
+
+for i in range(n):
+    board.append(list(input().split()))
+    for j in range(n):
+        if board[i][j] == 'T':
+            teachers.append((i, j))
+        if board[i][j] == 'X':
+            spaces.append((i, j))
+
+
+def watch(x, y, direction):
+    if direction == 0:
+        while y >= 0:
+            if board[x][y] == 'S':
+                return True
+            if board[x][y] == 'O':
+                return False
+            y -= 1
+
+    if direction == 1:
+        while y < n:
+            if board[x][y] == 'S':
+                return True
+            if board[x][y] == 'O':
+                return False
+            y += 1
+
+
+    if direction == 2:
+        while x >= 0:
+            if board[x][y] == 'S':
+                return True
+            if board[x][y] == 'O':
+                return False
+            x -= 1
+
+
+    if direction == 3:
+        while x < n:
+            if board[x][y] == 'S':
+                return True
+            if board[x][y] == 'O':
+                return False
+            x += 1
+    
+    return False
+
+def process():
+    for x, y in teachers:
+        for i in range(4):
+            if watch(x, y, i):
+                return True
+            
+    return False
+
+find = False
+
+for data in combinations(spaces, 3):
+    for x, y in data:
+        board[x][y] = "O"
+
+    if not process():
+        find = True
+        break
+
+    for x, y in data:
+        board[x][y] = 'X'
+
+
+
+if find:
+    print('YES')
+
+else:
+    print('NO')
+
