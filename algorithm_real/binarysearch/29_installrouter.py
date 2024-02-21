33eff34fe7f2404f1a0869abf3956131ad18c99f
sensival <alstjs883@naver.com>
2024-02-21 20:01:49 +0900

    Q29 공유기 설치

--- algorithm_real/binarysearch/29_installrouter.py
+++ algorithm_real/binarysearch/29_installrouter.py
@@ -38,6 +38,22 @@ result = 0
 
 while(start <= end):
     mid = (start + end) //2 
+    value = array[0]
+    count = 1
+
+    for i in range(1, n):
+        if array[i] >= value + mid:
+            value = array[i]
+            count += 1
     
+    if count >= c:
+        start = mid + 1
+        result = mid
+
+    else:
+        end = mid -1
+
+
+print(result)
 
 
