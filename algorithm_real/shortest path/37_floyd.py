a0125fb43fdfd91ce55ea1a39d0b976752a49758
sensival <alstjs883@naver.com>
2024-02-28 15:11:58 +0900

    Q27 플루이드

--- algorithm_real/shortest path/37_floyd.py
+++ algorithm_real/shortest path/37_floyd.py
@@ -38,3 +38,40 @@
 # 8 5 0 1 1
 # 10 7 13 0 3
 # 7 4 10 6 0
+
+INF = int(1e9) #10억
+
+# 노드 및 간선의 개수
+n = int(input())
+m = int(input())
+# 2차원 리스트를 만들고 초기화
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기자신 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+# 간선 저장
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # 대신에 같은 간선이면 비용 적은 간선만
+    if c < graph[a][b]:
+        graph[a][b] = c
+
+
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+
+for a in range(1, n + 1):
+    for b in range(1,  n + 1):
+        if graph[a][b] == INF:
+            print(0, end= " ")
+        else:
+            print(graph[a][b], end= " ")
+
+    print()
