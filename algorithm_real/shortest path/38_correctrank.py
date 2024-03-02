4da5caf64b52826fe64b7daa5819144fb2c6a349
sensival <alstjs883@naver.com>
2024-03-03 00:55:51 +0900

    Q38 정확한 순위

--- algorithm_real/shortest path/38_correctrank.py
+++ algorithm_real/shortest path/38_correctrank.py
@@ -34,3 +34,24 @@ for a in range(1, n + 1):
 for i in range(m):
     a, b = map(int, input().split())
     graph[a][b] = 1
+
+    
+# 점화식에 따라 플로이드 워셜 알고리즘
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+
+result = 0
+
+#각 학생을 번호에 따라 한 명씩 확인하며 도달 가능한지 확인
+for i in range(1, n + 1):
+    count = 0
+    for j in range(1, n + 1):
+        if graph[i][j] != INF or graph[j][i] != INF:
+            count += 1
+    if count == n:
+        result += 1
+
+print(result)
