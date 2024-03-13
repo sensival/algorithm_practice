36947dfff2556f6e27987368b6a16cf9f56fed35
sensival <alstjs883@naver.com>
2024-03-13 22:09:51 +0900

    Q41 여행 계획

--- algorithm_real/graph_theory/41_travelplan.py
+++ algorithm_real/graph_theory/41_travelplan.py
@@ -14,3 +14,48 @@
 # 출력 
 # YES
 
+# 서로소 집합 알고리즘
+# 루트노드확인
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+#합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+parent = [0] * (n + 1) # 부모테이블 초기화
+
+# 부모테이블 자기자신으로 
+for i in range(1, n + 1):
+    parent[i] = i
+
+# union
+for i in range(n):
+    data = list(map(int, input().split()))
+    for j in range(n):
+        if data[j] == 1:
+            union_parent(parent, i + 1, j + 1)
+
+# 여행 계획 입력받기
+plan = list(map(int, input().split()))
+
+result = True
+
+# 모든 노드의 루트노드가 동일한지 확인
+for i in range(m - 1):
+    if find_parent(parentm plan[i]) != find_parent(parent, plan[i + 1]):
+        result = False
+
+if result:
+    print("Yes")
+else:
+    print("No")
