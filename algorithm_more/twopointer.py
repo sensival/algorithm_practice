fd448bb5eef9c9c552933923eeb1dde01ec275ab
sensival <alstjs883@naver.com>
2024-05-05 21:44:38 +0900

    투포인터 알고리즘

---
+++ algorithm_more/twopointer.py
@@ -0,0 +1,42 @@
+# 특정한 합을 가지는 부분 연속 수열문제(배열에 음수가 있으면 불가)
+n = 5 # 데이터의 개수
+m = 5 # 찾고자 하는 부분합
+data = [1, 2, 3, 2, 5]
+
+count = 0
+interval_sum = 0
+end = 0
+
+for start in range(n):
+    while interval_sum < m and end < n:
+        interval_sum += data[end]
+        end += 1
+
+    if interval_sum == m:
+        count += 1
+    interval_sum -= data[start]
+
+print(count)
+
+
+# 정렬된 두 리스트이 합집합
+n, m = 3, 4
+a = [1, 3, 5]
+b = [2, 4, 6, 8]
+
+result = [0] * (n + m)
+i = 0
+j = 0
+k = 0
+
+while i < n or j < m:
+    if j >= m or (i < n and a[i] <= b[j]):
+        result[k] = a[i]
+        i += 1
+    else:
+        result[k] = b[j]
+        j += 1
+    k += 1
+
+for i in result:
+    print(i, end=' ')
