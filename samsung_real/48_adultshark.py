19c3cbb8e1e94b7d9093c0b6082c4fdbf59fa472
sensival <alstjs883@naver.com>
2024-05-11 00:31:27 +0900

    Q48 어른 상어

--- samsung_real/48_adultshark.py
+++ samsung_real/48_adultshark.py
@@ -131,3 +131,103 @@
 # 3 2 4 1
 # 1 4 2 3
 # 1 4 2 3
+# 예제 출력 4
+# -1
+
+n, m, k = map(int, input().split())
+
+# 상어의 위치
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
+
+# 상어의 방향
+directions = list(map(int, input().split()))
+
+# [냄새의 상어 번호, 냄새의 남은 시간]을 저장할 리스트
+smell = [[[0, 0]] * n for _ in range(n)]
+
+# 회전방향 우선순위 정보
+properties = [[] for _ in range(m)]
+
+# 각 상어의 회전 방향 우선순위정보
+for i in range(m):
+    for j in range(4):
+        properties[i].append(list(map(int, input().split())))
+                             
+# 이동가능한 4가지 방향
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# 모든 냄새 정보 업데이트
+def update_smell():
+    for i in range(n):
+        for j in range(n):
+            # 냄새가 존재하면 시간 1 감소
+            if smell[i][j][1] > 0:
+                smell[i][j][1] -= 1
+            # 상어가 존재하는 경우 냄새를 K로 설정
+            if array[i][j] != 0:
+                smell[i][j] = [array[i][j], k]
+
+# 모든 상어를 이동시키는 함수
+def move():
+    new_array = [[0] *  n for _ in range(n)]
+    for x in range(n):
+        for y in range(n):
+            if array[x][y] != 0:
+                direction = directions[array[x][y] - 1] # 현재 상어의 방향
+                found = False
+                # 일단 냄새가 존재하지 않는 곳이 있는지 확인
+                for index in range(4):
+                    nx = x + dx[properties[array[x][y] - 1][direction - 1][index]-1]
+                    ny = y + dy[properties[array[x][y] - 1][direction - 1][index]-1]
+                    if 0 <= nx and nx < n and 0 <= ny and ny < n:
+                        if smell[nx][ny][1] == 0:
+                            # 해당 상어의 방향 이동시키기
+                            directions[array[x][y] - 1] = properties[array[x][y] - 1][direction - 1][index]
+                            # 만약 이미 다른 상어가 있다면 번호가 낮은상어가 들어가도록
+                            if new_array[nx][ny] == 0:
+                                new_array[nx][ny] = array[x][y]
+                            else:
+                                new_array[nx][ny] = min(new_array[nx][ny], array[x][y])
+                            found = True
+                            break
+                
+                if found:
+                    continue
+                # 만약 주변에 냄새가 남아 있다면, 자신의 냄새가 있는 곳으로 이동
+                for index in range(4):
+                    nx = x + dx[properties[array[x][y] - 1][direction - 1][index]-1]
+                    ny = y + dy[properties[array[x][y] - 1][direction - 1][index]-1]
+                    if 0 <= nx and nx < n and 0 <= ny and ny < n:
+                        if smell[nx][ny][0] == array[x][y]:
+                            directions[array[x][y] - 1] = properties[array[x][y] - 1][direction - 1][index]
+                            new_array[nx][ny] = array[x][y]
+                            break
+    return new_array
+
+time = 0
+while True:
+    update_smell()
+    new_array = move()
+    array = new_array
+    time +=1
+
+    # 1번 상어만 남았는지 체크
+    check = True
+    for i in range(n):
+        for j in range(n):
+            if array[i][j] > 1:
+                check = False
+
+    if check:
+        print(time)
+        break
+
+    # 1000초 까지 끝나지 않았다면
+    if time >= 1000:
+        print(-1)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+        break
+
+
